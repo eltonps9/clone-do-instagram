ac7a66a2cfbb4e7e6ca333a829897c58e387ae85
Elton Passos <eltonps19@gmail.com>
2022-08-01 13:29:19 -0300

    algumas mudancas nos scripts e adicao de novas funcionalidades, adicionado verificação de login.

--- action/login_action.php
+++ action/login_action.php
@@ -1,1 +1,41 @@
-autenticando o login aqui
+<?php
+session_start();
+require_once "../config/connect.php";
+
+
+$val_usuario=filter_input(INPUT_POST, 'infor');
+$val_senha=filter_input(INPUT_POST, 'senha');
+
+// token a ser inserido no banco de dados
+$token=md5(time(),50);
+
+
+
+if(isset($val_usuario) && isset($val_senha) ){
+    //verificação do usuario
+    $sql=$pdo->prepare("SELECT * FROM login WHERE nome=:nome && senha=:senha");
+    $sql->bindValue(':nome',$val_usuario);
+    $sql->bindValue(':senha',$val_senha);
+    $sql->execute();
+
+    if($sql->rowCount()>0){
+        //atualização do token
+        $sql_update=$pdo->prepare("UPDATE login SET token=:token WHERE nome=:nome ");
+        $sql_update->bindValue('token',$token);
+        $sql_update->bindValue(':nome',$val_usuario);
+        $sql_update->execute();
+
+        $_SESSION['token']= $token;
+        header("Location:../pages/tela.php");
+        exit;
+    }else{
+        
+        $_SESSION['aviso']= "preencha as informações corretamente";
+    
+        header("Location:../index.php");
+        exit;
+    }
+    
+    
+}
+
---
+++ config/connect.php
@@ -0,0 +1,12 @@
+<?php
+
+$db_name="clone-instagram";
+$db_host="localhost";
+$db_pass="";
+$db_user="root";
+
+$pdo=new PDO("mysql:dbname=$db_name; host=$db_host","$db_user", "$db_pass");
+
+
+
+
